0cdbfe7e4adf1a1e8a9e5028086962264d57adbf
Joaquim Monserrat <joaquim.monserrat@i3television.es>
2023-10-18 08:33:50 +0200

    add two focusable buttons

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useFocusable } from '@noriginmedia/norigin-spatial-navigation';
+
+function Button() {
+  const { ref, focused, focusSelf } = useFocusable();
+
+  return (<TouchableOpacity
+    ref={ref}
+    onFocus={focusSelf}
+    style={focused ? styles.buttonFocused : styles.button}
+  >
+    <Text>Press me</Text>
+  </TouchableOpacity>);
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!asd</Text>
       <StatusBar style="auto" />
+      <Button />
+      <Button />
     </View>
   );
 }
@@ -17,4 +32,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonFocused: {
+    backgroundColor: "red",
+    marginBottom: 100
+  },
+  button: {
+    backgroundColor: "blue",
+    marginBottom:  100
+  }
 });
